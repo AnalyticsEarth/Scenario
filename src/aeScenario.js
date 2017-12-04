742416a7407cb719ec93c52c02468620f2c179d2
Steven Pressland <steven.pressland@qlik.com>
2017-12-04 11:27:50 +0000

    Added tooltip

--- src/aeScenario.js
+++ src/aeScenario.js
@@ -17,9 +17,11 @@ define( ["qlik", "text!./template.html","./definition","css!./aeScenarioStyle.cs
 			paint: function () {
 				return qlik.Promise.resolve();
 			},
-			controller: ['$scope', function ( $scope ) {
+			controller: ['$scope','luiTooltip', function ( $scope, tooltip ) {
 
 				var app = qlik.currApp(this);
+
+				console.log(tooltip);
 
 				$scope.userval = {};
 
@@ -49,12 +51,20 @@ define( ["qlik", "text!./template.html","./definition","css!./aeScenarioStyle.cs
 					return true;
 				};
 
-		// 		$scope.addTooltip = function(){
+				$scope.showTooltip = function(e){
+					console.log(e);
+					tooltip.show( {
+		        alignTo: e.currentTarget,
+		        dock: "right"
+		      } );
+				};
+
+		//  		$scope.addTooltip = function(){
 		// 			var docks = ["top", ["right", "left"], "bottom", ["left", "right"], "right"];
 	  // 			var tooltipTriggers = Array.prototype.slice.apply( document.querySelectorAll( ".tooltip-trigger" ) );
 	  // 			tooltipTriggers.forEach( function( element, idx ) {
 	  //   		var dock = docks[idx];
-	  //   var tooltip;
+	  //   var ttip;
 	  //   element.addEventListener( "mouseover", function() {
 	  //     var options = {
 	  //       alignTo: element,
@@ -63,15 +73,15 @@ define( ["qlik", "text!./template.html","./definition","css!./aeScenarioStyle.cs
 	  //     if ( idx === 4 ) {
 	  //       options.content = "<span>This text is bold and italic: <b><i>1337</i></b></span>"
 	  //     }
-	  //     tooltip = leonardoui.tooltip( options );
+	  //     ttip = tooltip.show( options );
 	  //   } );
 	  //   element.addEventListener( "mouseout", function() {
-	  //     if ( tooltip ) {
-	  //       tooltip.close();
+	  //     if ( ttip ) {
+	  //       //tooltip.close();
 	  //     }
 	  //   } );
 	  // } );
-		// 		};
+		//  		};
 
 
 
